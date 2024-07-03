1c909424a58df7205a6fa7a4c9a097c103244795
Dirceusljr <dirceusljr@gmail.com>
2024-07-02 21:50:11 -0300

    implementando testes no carrinho

---
+++ carrinho/test/carrinho.test.js
@@ -0,0 +1,34 @@
+import Item from '../item.js';
+import Carrinho from '../carrinho.js';
+
+describe('Testes do carrinho', () => {
+  it('Deve inicializar vazio', () => {
+    const carrinho = new Carrinho();
+
+    expect(carrinho.subtotal).toBeNull();
+  });
+
+  it('Deve adicionar itens no carrinho', () => {
+    const item1 = new Item('Banana', 2, 5);
+    const item2 = new Item('Maçã', 0.5, 1);
+
+    const carrinho = new Carrinho();
+
+    carrinho.adiciona(item1);
+    carrinho.adiciona(item2);
+
+    expect(typeof carrinho).toBe('object');
+
+    expect(carrinho.itens[0]).toBe(item1);
+    expect(carrinho.itens[1]).toBe(item2);
+
+    expect(carrinho.itens).toContain(item1);
+    expect(carrinho.itens).toContain(item2);
+  });
+
+  it('Deve ter a propriedade total na inicialização', () => {
+    const carrinho = new Carrinho();
+
+    expect(carrinho).toHaveProperty('total');
+  });
+});
